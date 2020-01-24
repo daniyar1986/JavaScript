632c9150205967c7b3eca949f3a92f855ea8e57a
DMyrzakanov <Danntu1986#>
2020-01-24 15:43:22 +0600

    Переменная

---
+++ variableExample.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let message;
+
+message = 'Hello';
+
+console.log(message);
+
+let message2 = 'Hello World';
+console.log(message2);
+
+let user = 'Danik', age = 33, message3 ='Hello3';
+
+let hello = 'Hello World';
+let message4 = hello;
+console.log(message4);
+
+let userName;
+let test123;
+
+let $ = 1;
+let _ = 2;
+
+//let 1a; it is wrong variable
+let apple;
+let APPLE;
+
+//let let = 5 it is wrong because is reserved
+//num = 5;
+
+const myBirthDay = '21.02.1986';
+//myBirthday = '01.01.2001' you can not rewrite constant
+
+const COLOR_RED = "#F00";
+let color = COLOR_RED;
+console.log(color);
+
+let admin;
+let name ='John';
+admin = name;
+console.log(admin);
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'Danik';
+
+const BIRTH_DATE = '21.02.1986';
+//const myAge = some_code(BIRTH_DATE);

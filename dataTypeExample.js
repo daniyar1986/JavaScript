26ac82790cfe3f9fcd7e43c59544092608222f89
DMyrzakanov <Danntu1986#>
2020-01-24 16:01:26 +0600

    Типы данных

---
+++ dataTypeExample.js
@@ -0,0 +1,44 @@
+let message = 'Hello';
+message = 12345;
+
+let n = 123;
+n = 123.45;
+console.log(1/0);
+console.log(Infinity);
+console.log("not number"/2);
+
+let str = "Hello";
+let str2 = ' World';
+
+let phrase = ` Result : ${str2}`;
+console.log(phrase);
+
+let name = "Danik";
+console.log(`Hi ${name}`);
+
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+let isGreater = 4>1;
+console.log(isGreater);
+
+let age = null;
+
+let x;
+console.log(x);
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof "foo");
+console.log(typeof Symbol("id"));
+console.log(typeof Math);
+console.log(typeof null);
+console.log(typeof alert);
+
+let testName = "Ilya";
+
+console.log( `hello ${1}` ); // ?
+
+console.log( `hello ${"testName"}` ); // ?
+
+console.log( `hello ${testName}` ); // ?

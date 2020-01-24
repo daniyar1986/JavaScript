460b0ad5479096578a8d44ef7bc64630741b7324
DMyrzakanov <Danntu1986#>
2020-01-24 16:35:16 +0600

    Преобразование типов

---
+++ typeConversionExample.js
@@ -0,0 +1,19 @@
+let value = true;
+
+console.log(value);
+
+value = String(value);
+console.log(typeof value);
+console.log("6"/"2");
+
+let str = "123";
+console.log(typeof str);
+
+let num = Number(str);
+console.log(typeof num);
+
+let age = Number("Text");
+console.log(age);
+
+console.log(Boolean(1));
+console.log(Boolean(0));

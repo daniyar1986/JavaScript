a15d1677929af3ef7fc3aa8e2f1e9cd1b66619e6
DMyrzakanov <Danntu1986#>
2020-01-28 12:02:54 +0600

    Функции-стрелки, основы

---
+++ ArrowFunction.js
@@ -0,0 +1,16 @@
+let sum = (a,b)=>a+b;
+
+console.log(sum(5,6));
+
+let double = n => n*2;
+console.log(double(3));
+
+let sayHello = () => console.log("Hello");
+sayHello();
+
+let sumABC = (a,b,c) =>{
+    let result = a+b+c;
+    return result;
+};
+
+console.log(sumABC(1,2,3));

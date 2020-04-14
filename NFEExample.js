4cb69e91c1b74f803ddf09135827bbff7e90899a
DMyrzakanov <Danntu1986#>
2020-04-14 17:53:27 +0600

    Named Function Expression

--- NFEExample.js
+++ NFEExample.js
@@ -4,3 +4,18 @@
 //  sayHi('Danik');
 
 
+let sayHi = function func(who) {
+    if (who){
+        console.log(`Hello, ${who}`)
+    } else {
+        func('Guest')
+    }
+};
+
+
+sayHi('Danik');
+sayHi();
+
+let welcome = sayHi;
+sayHi=null;
+welcome();
